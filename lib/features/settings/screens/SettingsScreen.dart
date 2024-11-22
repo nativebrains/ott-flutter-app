38fc15370df5587ab6bd5784d3bcac424ac9b7f5
Awais Mansha <awaismansha1998@gmail.com>
2024-11-22 18:26:02 +0500

    settings are working

--- lib/features/settings/screens/SettingsScreen.dart
+++ lib/features/settings/screens/SettingsScreen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../constants/routes_names.dart';
 import '../../../widgets/custom/custom_text.dart';
@@ -40,14 +45,140 @@ class _SettingsscreenState extends State<Settingsscreen> {
                     arguments:
                         const WebviewScreen(webviewType: WebviewType.PRIVACY));
               }),
-              getItem("Rate App", onItemClickCallback: () {}),
-              getItem("Share App", onItemClickCallback: () {}),
-              getItem("More App", onItemClickCallback: () {}),
+              getItem("Rate App", onItemClickCallback: () async {
+                await rateApp(context);
+              }),
+              getItem("Share App", onItemClickCallback: () async {
+                await shareApp();
+              }),
+              getItem("More App", onItemClickCallback: () async {
+                await openMoreApps();
+              }),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<void> openMoreApps() async {
+    try {
+      // Constructing the URL to redirect the user
+      String moreAppsLink;
+
+      if (Platform.isAndroid) {
+        // Redirect to the developer's Google Play Store page
+        // Replace with your developer page URL
+        moreAppsLink = 'https://play.google.com/store/apps/developer?id=Paybag';
+      } else if (Platform.isIOS) {
+        // Redirect to the developer's App Store page
+        // Replace with your developer page URL (App Store link)
+        moreAppsLink = 'https://apps.apple.com/developer/Paybag';
+      } else {
+        // Default behavior, if it's not Android or iOS (e.g., web)
+        moreAppsLink = 'https://play.google.com/store/apps/developer?id=Paybag';
+      }
+
+      // Launch the URL using url_launcher
+      final Uri url = Uri.parse(moreAppsLink);
+
+      if (await canLaunchUrl(url)) {
+        await launchUrl(url);
+      } else {
+        // If the URL cannot be launched, print an error or handle it accordingly
+        print('Could not launch $moreAppsLink');
+      }
+    } catch (e) {
+      // Handle any exceptions that might occur
+      print('Error opening more apps page: $e');
+    }
+  }
+
+  Future<void> shareApp() async {
+    try {
+      // Get the package name of the app
+      final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      final String packageName = packageInfo.packageName;
+
+      // Construct URLs for both platforms
+      String shareMessage =
+          "Hi,I would like to share Application which is used to play tv and movies free please download it from this link ";
+
+      // For Android: Use the Google Play Store link
+      final String playStoreLink =
+          'https://play.google.com/store/apps/details?id=$packageName';
+
+      // For iOS: Use the App Store link (replace 'YOUR_APP_ID' with your actual App Store ID)
+      final String appStoreLink = 'https://apps.apple.com/app/idYOUR_APP_ID';
+
+      // Platform-specific logic for sharing
+      String shareContent;
+      if (Platform.isAndroid) {
+        shareContent = '$shareMessage$playStoreLink'; // For Android
+      } else if (Platform.isIOS) {
+        shareContent = '$shareMessage$appStoreLink'; // For iOS
+      } else {
+        shareContent =
+            '$shareMessage$playStoreLink'; // Default to Android link for other platforms
+      }
+
+      // Share content using share_plus package
+      await Share.share(shareContent);
+    } catch (e) {
+      // Handle any errors that might occur
+      print('Error sharing the app: $e');
+    }
+  }
+
+  Future<void> rateApp(BuildContext context) async {
+    try {
+      // Get the package name of the app
+      final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      final String packageName = packageInfo.packageName;
+
+      // Construct URLs
+      final String playStoreUrl =
+          'https://play.google.com/store/apps/details?id=$packageName';
+      final String playStoreAppUrl = 'market://details?id=$packageName';
+      final String appStoreUrl =
+          'https://apps.apple.com/app/idYOUR_APP_ID'; // Replace `YOUR_APP_ID` with your App Store ID
+
+      if (Platform.isAndroid) {
+        // Android-specific logic
+        final Uri appUri = Uri.parse(playStoreAppUrl);
+        if (await canLaunchUrl(appUri)) {
+          await launchUrl(appUri);
+        } else {
+          final Uri webUri = Uri.parse(playStoreUrl);
+          if (await canLaunchUrl(webUri)) {
+            await launchUrl(webUri, mode: LaunchMode.externalApplication);
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Unable to open the Play Store.')),
+            );
+          }
+        }
+      } else if (Platform.isIOS) {
+        // iOS-specific logic
+        final Uri appUri = Uri.parse(appStoreUrl);
+        if (await canLaunchUrl(appUri)) {
+          await launchUrl(appUri, mode: LaunchMode.externalApplication);
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Unable to open the App Store.')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Unsupported platform.')),
+        );
+      }
+    } catch (e) {
+      // Handle errors gracefully
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred: $e')),
+      );
+    }
   }
 
   Widget getItem(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,11 @@ import device_info_plus
 import file_selector_macos
 import firebase_core
 import flutter_image_compress_macos
+import package_info_plus
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,7 +21,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
