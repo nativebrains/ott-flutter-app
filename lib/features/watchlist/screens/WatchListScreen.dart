03fad03ca4b4e1d70f810954bc18cc4b8f172253
Awais Mansha <awaismansha1998@gmail.com>
2024-11-23 23:29:53 +0500

    working perfect

--- lib/features/watchlist/screens/WatchListScreen.dart
+++ lib/features/watchlist/screens/WatchListScreen.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../constants/constants.dart';
+import '../../dashboard/widgets/CustomHorizontalCard.dart';
 
 class Watchlistscreen extends StatefulWidget {
   const Watchlistscreen({super.key});
@@ -8,14 +14,42 @@ class Watchlistscreen extends StatefulWidget {
 }
 
 class _WatchlistscreenState extends State<Watchlistscreen> {
+  List<Widget> _items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 10; i++) {
+      _items.add(
+        Row(
+          children: List.generate(
+            2,
+            (index) => Expanded(
+              child: Container(
+                  margin: EdgeInsets.all(6),
+                  child: Customhorizontalcard(
+                      isPremium: true,
+                      showTitle: true,
+                      url: Constants.imgHorizontalList[Random()
+                          .nextInt(Constants.imgHorizontalList.length)])),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Watch List Screen',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 12.sp),
+            ..._items,
+            SizedBox(height: 12.sp)
+          ],
         ),
       ),
     );
