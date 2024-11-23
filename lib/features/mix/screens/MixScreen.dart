7e989790cb0d6eed53fbf07850442645985bd20d
Awais Mansha <awaismansha1998@gmail.com>
2024-11-24 00:04:07 +0500

    working perfect

--- lib/features/mix/screens/MixScreen.dart
+++ lib/features/mix/screens/MixScreen.dart
@@ -1,9 +1,12 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islamforever/features/common/enums/MediaContentType.dart';
 import 'package:islamforever/features/dashboard/providers/DashboardProvider.dart';
 import 'package:provider/provider.dart';
 
+import '../../../constants/app_colors.dart';
 import '../../../constants/constants.dart';
 import '../../dashboard/widgets/CustomHorizontalCard.dart';
 import '../../dashboard/widgets/CustomVerticalCard.dart';
@@ -16,25 +19,39 @@ class Mixscreen extends StatefulWidget {
 }
 
 class _MixscreenState extends State<Mixscreen> {
+  late DashboardProvider _dashboardProvider;
+
   List<Widget> _items = [];
-  bool isVertical = false;
 
   @override
   void initState() {
     super.initState();
     refreshItemsList();
+    _dashboardProvider = Provider.of<DashboardProvider>(context, listen: false);
+    _dashboardProvider.addListener(refreshItemsList);
+  }
+
+  @override
+  void dispose() {
+    _dashboardProvider.removeListener(refreshItemsList);
+    super.dispose();
   }
 
   void refreshItemsList() {
+    _items.clear(); // Clear the list
     for (int i = 0; i < 10; i++) {
       _items.add(
         Row(
           children: List.generate(
-            isVertical ? 3 : 2,
+            DashboardProvider.selectedMixScreenContentType ==
+                    MediaContentType.movies
+                ? 3
+                : 2,
             (index) => Expanded(
               child: Container(
                   margin: EdgeInsets.all(6),
-                  child: isVertical
+                  child: DashboardProvider.selectedMixScreenContentType ==
+                          MediaContentType.movies
                       ? Customverticalcard(
                           isPremium: false,
                           url: Constants.imgVerticalList[Random()
@@ -49,18 +66,20 @@ class _MixscreenState extends State<Mixscreen> {
         ),
       );
     }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     final dashboardProvider = Provider.of<DashboardProvider>(context);
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          DashboardProvider.selectedMixScreenContentTypeName,
-          style: TextStyle(
-              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+      backgroundColor: ColorCode.blackColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.sp, vertical: 20.sp),
+          child: Column(
+            children: [..._items],
+          ),
         ),
       ),
     );
