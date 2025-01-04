a0ce478587d9b958d9494f4cc277400e927b9e9d
Awais Mansha <awaismansha1998@gmail.com>
2025-01-04 23:20:24 +0500

    Search is working

--- lib/features/dashboard/screens/SearchScreen.dart
+++ lib/features/dashboard/screens/SearchScreen.dart
@@ -53,174 +53,195 @@ class _SearchscreenState extends State<Searchscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorCode.bgColor,
-      body: CustomScrollView(
-        slivers: [
-          // SliverAppBar with snapping and floating behavior
-          SliverAppBar(
-            automaticallyImplyLeading: true,
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.orange, Colors.pink],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              automaticallyImplyLeading: true,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.orange, Colors.pink],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
                 ),
               ),
-            ),
-            title: Text(
-              "Search",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+              title: Text(
+                "Search",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
               ),
+              centerTitle: false,
+              floating: true,
+              snap: true,
+              expandedHeight: 55.0, // Set height for expanded AppBar
             ),
-            centerTitle: false,
-            floating: true,
-            snap: true,
-            expandedHeight: 55.0, // Set height for expanded AppBar
-          ),
-          // SliverList for your content
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                if (searchResults.isEmpty)
+          ];
+        }, // SliverList for your content
+        body: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (searchResults.isEmpty && !_isLoading)
                   getNoItemsFound()
                 else
                   getItemsList(),
-                if (_isLoading) const LoaderWidget(),
               ],
             ),
+            if (_isLoading) const LoaderWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getNoItemsFound() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300.sp,
+          ),
+          Icon(
+            Icons.search,
+            size: 80.sp,
+            color: Colors.white,
+          ),
+          SizedBox(height: 10),
+          Text(
+            'No items found',
+            style: TextStyle(fontSize: 18, color: Colors.white),
           ),
         ],
       ),
     );
   }
 
-  Widget getNoItemsFound() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: 300.sp,
-        ),
-        Icon(
-          Icons.search,
-          size: 80.sp,
-          color: Colors.white,
-        ),
-        SizedBox(height: 10),
-        Text(
-          'No items found',
-          style: TextStyle(fontSize: 18, color: Colors.white),
-        ),
-      ],
-    );
-  }
-
   Widget getShowsList(List<Widget> showsList) {
     if (showsList.isEmpty) return Container();
-    return Column(
-      children: [
-        SizedBox(height: 12.sp),
-        Text(
-          "Shows",
-          style: GoogleFonts.poppins(
-            color: ColorCode.whiteColor,
-            fontWeight: FontWeight.w600,
-            fontSize: 18.sp,
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 12.sp),
+          Text(
+            "Shows",
+            style: GoogleFonts.poppins(
+              color: ColorCode.whiteColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.sp,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: showsList,
+          SizedBox(height: 12.sp),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: showsList,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-      ],
+          SizedBox(height: 12.sp),
+        ],
+      ),
     );
   }
 
   Widget getMoviesList(List<Widget> moviesList) {
     if (moviesList.isEmpty) return Container();
-    return Column(
-      children: [
-        SizedBox(height: 12.sp),
-        Text(
-          "Movies",
-          style: GoogleFonts.poppins(
-            color: ColorCode.whiteColor,
-            fontWeight: FontWeight.w600,
-            fontSize: 18.sp,
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 12.sp),
+          Text(
+            "Movies",
+            style: GoogleFonts.poppins(
+              color: ColorCode.whiteColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.sp,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: moviesList,
+          SizedBox(height: 12.sp),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: moviesList,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-      ],
+          SizedBox(height: 12.sp),
+        ],
+      ),
     );
   }
 
   Widget getSportsList(List<Widget> sportsList) {
     if (sportsList.isEmpty) return Container();
-    return Column(
-      children: [
-        SizedBox(height: 12.sp),
-        Text(
-          "Sports",
-          style: GoogleFonts.poppins(
-            color: ColorCode.whiteColor,
-            fontWeight: FontWeight.w600,
-            fontSize: 18.sp,
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 12.sp),
+          Text(
+            "Sports",
+            style: GoogleFonts.poppins(
+              color: ColorCode.whiteColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.sp,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: sportsList,
+          SizedBox(height: 12.sp),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: sportsList,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-      ],
+          SizedBox(height: 12.sp),
+        ],
+      ),
     );
   }
 
   Widget getLiveTvList(List<Widget> liveTvList) {
     if (liveTvList.isEmpty) return Container();
-    return Column(
-      children: [
-        SizedBox(height: 12.sp),
-        Text(
-          "Live TV",
-          style: GoogleFonts.poppins(
-            color: ColorCode.whiteColor,
-            fontWeight: FontWeight.w600,
-            fontSize: 18.sp,
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 12.sp),
+          Text(
+            "Live TV",
+            style: GoogleFonts.poppins(
+              color: ColorCode.whiteColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 18.sp,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: liveTvList,
+          SizedBox(height: 12.sp),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: liveTvList,
+            ),
           ),
-        ),
-        SizedBox(height: 12.sp),
-      ],
+          SizedBox(height: 12.sp),
+        ],
+      ),
     );
   }
 
@@ -228,47 +249,60 @@ class _SearchscreenState extends State<Searchscreen> {
     for (var item in searchResults) {
       if (item is ItemShowModel) {
         showsList.add(
-          Customhorizontalcard(
-            isPremium: item.isPremium,
-            showTitle: true,
-            url: item.showImage ?? "",
-            id: item.showId,
-            title: item.showName,
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Customhorizontalcard(
+              isPremium: item.isPremium,
+              showTitle: true,
+              url: item.showImage ?? "",
+              id: item.showId,
+              title: item.showName,
+            ),
           ),
         );
       } else if (item is ItemMovieModel) {
         moviesList.add(
-          Customverticalcard(
-            isPremium: item.isPremium ?? false,
-            url: item.movieImage ?? "",
-            id: item.movieId,
-            title: item.movieName,
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Customverticalcard(
+              isPremium: item.isPremium ?? false,
+              url: item.movieImage ?? "",
+              id: item.movieId,
+              title: item.movieName,
+            ),
           ),
         );
       } else if (item is ItemSportModel) {
         sportsList.add(
-          Customhorizontalcard(
-            isPremium: item.isPremium,
-            showTitle: true,
-            url: item.sportImage ?? "",
-            id: item.sportId,
-            title: item.sportName,
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Customhorizontalcard(
+              isPremium: item.isPremium,
+              showTitle: true,
+              url: item.sportImage ?? "",
+              id: item.sportId,
+              title: item.sportName,
+            ),
           ),
         );
       } else if (item is ItemLiveTVModel) {
         liveTvList.add(
-          Customhorizontalcard(
-            isPremium: item.isPremium,
-            showTitle: true,
-            url: item.tvImage ?? "",
-            id: item.tvId,
-            title: item.tvName,
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Customhorizontalcard(
+              isPremium: item.isPremium,
+              showTitle: true,
+              url: item.tvImage ?? "",
+              id: item.tvId,
+              title: item.tvName,
+            ),
           ),
         );
       }
     }
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         getShowsList(showsList),
         getMoviesList(moviesList),
