ab398dd368c914ff3537be1ae105e3134c637083
Awais Mansha <awaismansha1998@gmail.com>
2025-01-16 23:34:17 +0500

    youtube player is working perfect

--- lib/features/details/screens/VideroPlayerScreen.dart
+++ lib/features/details/screens/VideroPlayerScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islamforever/constants/app_colors.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
@@ -36,7 +37,7 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideroplayerscreenState extends State<VideoPlayerScreen> {
-  late YoutubePlayerController _controller;
+  late YoutubePlayerController _youtubeController;
   late DetailsProvider detailsProvider;
   bool _isLoading = false;
   @override
@@ -53,17 +54,9 @@ class _VideroplayerscreenState extends State<VideoPlayerScreen> {
     });
 
     if (widget.videoPlayerScreenArguments.videoPlayerType ==
-        VideoPlayerType.Youtube)
-      _controller = YoutubePlayerController(
-        initialVideoId: widget.videoPlayerScreenArguments.videoId.toString(),
-        flags: YoutubePlayerFlags(
-          autoPlay: true,
-          mute: false, // Set to false to unmute the video
-          loop: true, // Set to true to loop the video
-          isLive: false, // Set to true for live videos
-          forceHD: false, // Set to true to force HD quality
-        ),
-      );
+        VideoPlayerType.Youtube) {
+      prepareYoutubeController();
+    }
 
     setState(() {
       _isLoading = false;
@@ -80,32 +73,68 @@ class _VideroplayerscreenState extends State<VideoPlayerScreen> {
             if (!_isLoading)
               if (widget.videoPlayerScreenArguments.videoPlayerType ==
                   VideoPlayerType.Youtube)
-                YoutubePlayer(
-                  controller: _controller,
-                  showVideoProgressIndicator: true,
-                  progressIndicatorColor: Colors.amber,
-                  progressColors: const ProgressBarColors(
-                    playedColor: Colors.amber,
-                    handleColor: Colors.amberAccent,
-                  ),
-                  onReady: () {
-                    // _controller.addListener(listener);
-                  },
-                  bottomActions: [
-                    CurrentPosition(),
-                    ProgressBar(
-                      isExpanded: true,
-                    ),
-                    RemainingDuration(),
-                    FullScreenButton(
-                      controller: _controller,
-                    ),
-                  ],
-                ),
+                _getYoutubePlayer(),
             if (_isLoading) const LoaderWidget(),
           ],
         ),
       ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _youtubeController.dispose();
+    super.dispose();
+  }
+
+  void prepareYoutubeController() {
+    _youtubeController = YoutubePlayerController(
+      initialVideoId: widget.videoPlayerScreenArguments.videoId.toString(),
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false, // Set to false to unmute the video
+        loop: true, // Set to true to loop the video
+        isLive: false, // Set to true for live videos
+        forceHD: false, // Set to true to force HD quality
+        disableDragSeek: false,
+      ),
+    );
+  }
+
+  _getYoutubePlayer() {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _youtubeController,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.greenAccent,
+        progressColors: const ProgressBarColors(
+          playedColor: Colors.greenAccent,
+          handleColor: Colors.greenAccent,
+        ),
+        bottomActions: [
+          SizedBox(
+            width: 12.sp,
+          ),
+          CurrentPosition(),
+          ProgressBar(
+            isExpanded: true,
+          ),
+          RemainingDuration(),
+          FullScreenButton(
+            controller: _youtubeController,
+          ),
+          SizedBox(
+            width: 12.sp,
+          ),
+        ],
+      ),
+      builder: (context, player) {
+        return Column(
+          children: [
+            Expanded(child: player), // This will take up the full height
+          ],
+        );
+      },
     );
   }
 }
